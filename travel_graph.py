c52a6c649d656e7369fc91e9681070204597a613
sophiavv <meowypurr@yahoo.com>
2014-01-17 21:41:56 -0500

    added command loop and usage string

--- travel_graph.py
+++ travel_graph.py
@@ -36,7 +36,7 @@ class Graph():
     def distance(self, path):
         # path is list of (to_b, date, price)
         return sum( price for to_b, date, price in path )
-
+"""
 g = Graph()
 fname = sys.argv[1]
 start = sys.argv[2]
@@ -56,4 +56,45 @@ for node in shortest_path:
     print "%s %s $%.2f"%node
 total_price = g.distance(shortest_path)
 print "total price: $%.2f"%total_price
+"""
+def print_usage():
+    print "Usage:"
+    print "\tadd <from> <to> <date> <price> | <id>"
+    print ("\tadds a node to the graph. If node was previously removed, re-add"
+            " using <id>.\n")
+    print "\texport <filename>"
+    print "\texports current graph to filename.\n"
+    print "\timport <filename>"
+    print "\timports a graph from filename.\n"
+    print "\tlist [--removed | --all]"
+    print "\tlists current edges. "
+    print "\tIf --removed option is set, removed edges will be displayed. "
+    print "\tIf --all option is set, all edges will be displayed.\n"
+    print "\tpath <start> <end>"
+    print "\tdisplays shortest path from <start> to <end>.\n"
+    print "\tquit"
+    print "\tquits the program.\n"
+    print "\tremove <from> <to> <date> <price> | <id>"
+    print "\tremoves the specified node from the graph."
+
+def parse_cmd(cmd):
+    pass
+
+IMPORT = 'import'
+PATH = 'path'
+ADD = 'add'
+REMOVE = 'remove'
+LIST = 'list'
+EXPORT = 'export'
+QUIT = 'quit'
+CMD_LIST = [IMPORT, EXPORT, PATH, LIST, ADD, REMOVE, QUIT] 
+PROMPT = '"._.)> '
+g = Graph()
+cmd = ''
+while cmd != QUIT:
+    cmd = raw_input(PROMPT)
+    if cmd not in CMD_LIST and cmd != '':
+        print_usage()
+        continue
+    parsed_cmd = parse_cmd(cmd)
 
