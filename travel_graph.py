2f0f358058c286c543647a961118f0857a6a9df0
sophiavv <meowypurr@yahoo.com>
2014-01-18 00:17:52 -0500

    added rest of methods for interactive tool

--- travel_graph.py
+++ travel_graph.py
@@ -1,13 +1,33 @@
-import sys
+#!/usr/bin/python
+
 import csv
 
 class Graph():
     def __init__(self):
         self.graph = {}
+        self.next_id = 0
+    def list_edges(self, show_enabled=True, show_disabled=False):
+        for from_a, edges in self.graph.items():
+            for to_b, edge in edges.items():
+                edge_id, date, price, enabled = edge
+                enabled_str = 'enabled' if enabled else 'disabled'
+                edge_str = "%d:\t%s->%s\t\t%s\t$%.2f\t%s"%(edge_id, from_a, to_b,
+                        date, price, enabled_str)
+                if enabled and show_enabled:
+                    print edge_str
+                elif not enabled and show_disabled:
+                    print edge_str
+    def get_edge(self, target_edge_id):
+        for from_a, edges in self.graph.items():
+            for to_b, edge in edges.items():
+                edge_id, date, price, enabled = edge
+                if edge_id == target_edge_id:
+                    return edge
     def add_edge(self, from_a, to_b, date, price):
         if from_a not in self.graph:
             self.graph[from_a] = {}
-        self.graph[from_a][to_b] = (date, price)
+        self.graph[from_a][to_b] = [self.next_id, date, price, True]
+        self.next_id += 1
     def neighbors(self, from_a):
         if from_a not in self.graph:
             return {}
@@ -20,7 +40,9 @@ class Graph():
             if neighbors == {}:
                 return []
             for n, val in neighbors.items():
-                date, price = val
+                edge_id, date, price, enabled = val
+                if not enabled:
+                    continue
                 node = (n, date, price)
                 if node not in current_path:
                     new_path = current_path + [node]
@@ -36,28 +58,16 @@ class Graph():
     def distance(self, path):
         # path is list of (to_b, date, price)
         return sum( price for to_b, date, price in path )
-"""
-g = Graph()
-fname = sys.argv[1]
-start = sys.argv[2]
-end = sys.argv[3]
-with open(fname, 'rb') as gfile:
-    reader = csv.reader(gfile)
-    for row in reader:
-        if len(row) != 4:
-            continue
-        from_a = row[0]
-        to_b = row[1]
-        date = row[2]
-        price = float(row[3])
-        g.add_edge(from_a, to_b, date, price)
-shortest_path = g.shortest_path(start, end)
-for node in shortest_path:
-    print "%s %s $%.2f"%node
-total_price = g.distance(shortest_path)
-print "total price: $%.2f"%total_price
-"""
-def print_usage():
+    def __str__(self):
+        graph_str = ''
+        for from_a, edges in self.graph.items():
+            for to_b, edge in edges.items():
+                edge_id, date, price, enabled = edge
+                if enabled:
+                    graph_str += "%s,%s,%s,%.2f\n"%(from_a, to_b, date, price)
+        return graph_str
+
+def print_usage(args=None, g=None):
     print "Usage:"
     print "\tadd <from> <to> <date> <price> | <id>"
     print ("\tadds a node to the graph. If node was previously removed, re-add"
@@ -77,8 +87,96 @@ def print_usage():
     print "\tremove <from> <to> <date> <price> | <id>"
     print "\tremoves the specified node from the graph."
 
-def parse_cmd(cmd):
-    pass
+def import_graph(args, g):
+    if len(args) != 2:
+        return False
+    fname = args[1]
+    with open(fname, 'rb') as gfile:
+        reader = csv.reader(gfile)
+        for row in reader:
+            if len(row) != 4:
+                continue
+            from_a = row[0]
+            to_b = row[1]
+            date = row[2]
+            price = float(row[3])
+            g.add_edge(from_a, to_b, date, price)
+    print "Successfully imported graph from %s !"%fname
+
+def export_graph(args, g):
+    if len(args) != 2:
+        return False
+    fname = args[1]
+    with open(fname, 'w') as outfile:
+        outfile.write(str(g))
+        outfile.close()
+    print "Successfully wrote graph to %s !"%fname
+    return True
+    
+def add_edge_to_graph(args, g):
+    n_args = len(args)
+    try:
+        if n_args == 2:
+            edge_id = int(args[1])
+            edge = g.get_edge(edge_id)
+            if edge:
+                edge[3] = True
+                return True
+            else:
+                return False
+        elif n_args == 5:
+            from_a = args[1]
+            to_b = args[2]
+            date = args[3]
+            price = float(args[4])
+            g.add_edge(from_a, to_b, date, price)
+            return True
+        else:
+            return False
+    except ValueError:
+        return False
+
+def remove_edge_from_graph(args, g):
+    try:
+        if len(args) == 2:
+            edge_id = int(args[1])
+            edge = g.get_edge(edge_id)
+            if edge:
+                edge[3] = False
+                return True
+            else:
+                return False
+        else:
+            return False
+    except ValueError:
+        return False
+
+def find_shortest_path(args, g):
+    if len(args) != 3:
+        return False
+    start = args[1]
+    end = args[2]
+    shortest_path = g.shortest_path(start, end)
+    total_price = g.distance(shortest_path)
+    print "%s-->"%start
+    for to_b, date, price in shortest_path:
+        print "%s\t\t%s\t\t$%.2f"%(to_b, date, price)
+    print "TOTAL PRICE:\t$%.2f"%total_price
+
+def list_edges(args, g):
+    if len(args) > 2:
+        return False
+    if len(args) == 1:
+        g.list_edges()
+    else:
+        arg = args[1]
+        if arg == '--removed':
+            g.list_edges(False, True)
+        elif arg == '--all':
+            g.list_edges(True, True)
+        else:
+            return False
+    return True
 
 IMPORT = 'import'
 PATH = 'path'
@@ -87,14 +185,38 @@ REMOVE = 'remove'
 LIST = 'list'
 EXPORT = 'export'
 QUIT = 'quit'
-CMD_LIST = [IMPORT, EXPORT, PATH, LIST, ADD, REMOVE, QUIT] 
-PROMPT = '"._.)> '
-g = Graph()
-cmd = ''
-while cmd != QUIT:
-    cmd = raw_input(PROMPT)
-    if cmd not in CMD_LIST and cmd != '':
-        print_usage()
-        continue
-    parsed_cmd = parse_cmd(cmd)
+USAGE = 'usage'
+CMD_LIST = [USAGE, IMPORT, EXPORT, PATH, LIST, ADD, REMOVE, QUIT] 
 
+def parse_and_execute_cmd(cmd, g):
+    args = cmd.split(' ')
+    func_dict = {  
+        # dictionary containing functions for executing commands.
+        # each takes args as its argument and return True or False.
+        IMPORT  : import_graph,
+        EXPORT  : export_graph,
+        ADD     : add_edge_to_graph,
+        REMOVE  : remove_edge_from_graph,
+        PATH    : find_shortest_path,
+        LIST    : list_edges,
+        USAGE   : print_usage,
+        QUIT    : None
+    }
+    cmd_func = func_dict.get(args[0], print_usage)
+    if cmd_func:
+        return cmd_func(args, g)
+    else:
+        return True
+
+def main():
+    g = Graph()
+    PROMPT = '"._.)> '
+    cmd = ''
+    while cmd != QUIT:
+        cmd = raw_input(PROMPT)
+        if cmd == '':
+            continue
+        success = parse_and_execute_cmd(cmd, g)
+        
+if __name__ == '__main__':
+    main()
